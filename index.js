2b3cc25d7a09683a02abafaf4f69b58a7f0f73d0
Nathan Le <nathan.le@tekos.net>
2021-10-22 16:35:47 +0700

    Capitalize and add comment for calc function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const submitButton = document.getElementById('submitBtn');
 const resultNumber = document.getElementById('result');
 
+/**
+ * Calc sum of number 1 and 2
+ * @returns number
+ */
 const calcAdd = () => {
   const number1 = document.getElementById('number1').value;
   const number2 = document.getElementById('number2').value;
-
   return number1 * 1 - number2 * 1; // Have to *1 to convert string to number
 };
 
